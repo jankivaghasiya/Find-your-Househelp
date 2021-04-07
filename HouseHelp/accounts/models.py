3e6b36cab55d6fde7ebb79d91664e9f5fc899a89
Bharg Trivedi <bhargtrivedi2001@gmail.com>
2021-04-07 12:54:30 +0530

    login authentication added

--- HouseHelp/accounts/models.py
+++ HouseHelp/accounts/models.py
@@ -3,31 +3,26 @@ from django.contrib.auth.models import User
 from django.db.models.fields.related import OneToOneField
 
 class Works(models.Model):
-    brooming = models.BooleanField(default='false')
-    mopping = models.BooleanField(default='false')
-    utensil_cleaning = models.BooleanField(default='false')
-    washing_clothes = models.BooleanField(default='false')
-    cooking = models.BooleanField(default='false')
-    babysitting = models.BooleanField(default='false')
+    brooming = models.BooleanField(default='False')
+    mopping = models.BooleanField(default='True')
+    utensil_cleaning = models.BooleanField(default='False')
+    washing_clothes = models.BooleanField(default='False')
+    cooking = models.BooleanField(default='False')
+    babysitting = models.BooleanField(default='False')
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    password = models.CharField(max_length=20)
-    email = models.EmailField(max_length=254)
     contact_no = models.IntegerField()
     address = models.TextField()
     
 class Helper(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
     contact_no = models.IntegerField()
     address = models.TextField()
-    gender = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50, default='')
     dob = models.DateField()
-    password = models.CharField(max_length=20)
-    years_of_experience = models.IntegerField()
-    religion = models.CharField(max_length=50)
+    years_of_experience = models.IntegerField(default=2)
+    religion = models.CharField(max_length=50, default="")
     works = models.OneToOneField(Works, on_delete=models.CASCADE)
 
 class Booking(models.Model):
--- HouseHelp/accounts/views.py
+++ HouseHelp/accounts/views.py
@@ -1,24 +1,43 @@
+from django.core.checks import messages
 from django.shortcuts import render
-from ..accounts.models import Customer
+from .models import Customer, Helper, Works
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 def helper_register(request):
     if request.method == 'POST':
         fname = request.POST['fname']
         lname = request.POST['lname']
-        address = request.POST['']
+        address = request.POST['address']
         phone = request.POST['phone']
+        dob = request.POST['dob']
         password1 = request.POST['pass']
         password2 = request.POST['cpass']
         email = request.POST['email']
-        user = User.objects.create()
-        customer = Customer(user = user)
+        username = request.POST['username']
+        user = User.objects.create(first_name=fname, last_name=lname, username=username, password=password1, email=email)
+        works = Works()
+        works.save()
+        helper = Helper(user=user, address=address, contact_no=phone, dob=dob, works=works)
+        helper.save()
+        message = "User created"
+        return render(request, "message.html", {'message':message})
     return render(request, "register_as_helper.html")
 
 def customer_register(request):
     return render(request, "register_as_customer.html")
 
 def login(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        username = request.POST['username']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            message = "User is logged in"
+        else :
+            message = "Invalid username or password"
+        return render(request, "message.html", {'message':message})
     return render(request, "login.html")
 
